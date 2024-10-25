8febac3d0f112bfa03b79f93cc21a62193b75287
n0tx <rcandra91@msn.com>
2024-10-25 17:22:33 +0700

    change admin, user landing page header

--- user/index.php
+++ user/index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>user page</title>
+    <title>User Page</title>
 </head>
 <body>
-    <h1>Hello this is user page!</h1>
+    <h1>Welcome riki!</h1>
 </body>
 </html>
